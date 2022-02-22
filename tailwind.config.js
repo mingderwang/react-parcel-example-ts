fefb0ace0e92d212244a41093dab030b90430876
Ming-der Wang <mingderwang@gmail.com>
2022-02-22 10:02:03 +0800

    test menu bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,13 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
 };
